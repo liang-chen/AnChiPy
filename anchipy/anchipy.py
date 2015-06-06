0afb0b9675625e91f8b90355e26c1da918b47b71
Liang Chen <chen348@indiana.edu>
2015-06-06 13:05:06 -0400

    debug margins and borders

--- anchipy/anchipy.py
+++ anchipy/anchipy.py
@@ -24,7 +24,7 @@ def locate_words(uni_words):
     ll = []
 #    print n, " length of words"
     tot_len = n*font_size
-    ncols = int(ceil(float(tot_len)/img_height)); 
+    ncols = int(ceil(float(tot_len)/float(img_height-2*tot_margin))); 
     #set global image width
     img_width = ncols*(font_size+2*udl_margin+udl_width) + 2*tot_margin
 #    print ncols, img_width, img_height
@@ -41,6 +41,45 @@ def locate_words(uni_words):
 
     return ll
 
+def add_border(draw):
+    #add horizontal ones:
+    #outter
+    i = outter_border_margin
+    j1 = i
+    j2 = img_width - j1 - outter_border_width/2
+    draw.line((j1,i,j2,i),fill = (255,0,0,0), width = outter_border_width)
+
+    i = img_height - outter_border_margin - outter_border_width/2
+    draw.line((j1,i,j2,i),fill = (255,0,0,0), width = outter_border_width)
+    
+    #inner
+    i = outter_border_margin + outter_border_width + inner_border_margin
+    j1 = i
+    j2 = img_width - j1 - inner_border_width
+    draw.line((j1,i,j2,i),fill = (255,0,0,0), width = inner_border_width)
+
+    i = img_height - outter_border_margin - outter_border_width - inner_border_margin - inner_border_width
+    draw.line((j1,i,j2,i),fill = (255,0,0,0), width = inner_border_width)
+
+    ##vertical ones
+    #outter
+    j = outter_border_margin
+    i1 = j
+    i2 = img_height - i1 - outter_border_width/2
+    draw.line((j,i1,j,i2), fill = (255,0,0,0), width = outter_border_width)
+
+    j = img_width - outter_border_margin - outter_border_width/2
+    draw.line((j,i1,j,i2), fill = (255,0,0,0), width = outter_border_width)
+
+    #inner
+    j = outter_border_margin + outter_border_width + inner_border_margin
+    i1 = j
+    i2 = img_height - i1 - inner_border_width
+    draw.line((j,i1,j,i2), fill = (255,0,0,0), width = inner_border_width)
+
+    j = img_width - j - inner_border_width
+    draw.line((j,i1,j,i2), fill = (255,0,0,0), width = inner_border_width)
+    
 def render_whole_page(uni_words, loc_list):
     global font_size,img_height,img_width,udl_width,udl_margin
     if img_width == 0:
@@ -50,10 +89,10 @@ def render_whole_page(uni_words, loc_list):
     font = ImageFont.truetype(resource_filename(__name__,'cwTeXQKaiZH-Medium.ttf'), size = font_size, encoding = "unic")
     for i in range(len(uni_words)):
         draw.text(loc_list[i], jtof(uni_words[i]),font=font,fill=(0,0,0,0))
-        if loc_list[i][1] == 0:
+        if loc_list[i][1] == tot_margin:
             ##draw red vertical line
-            draw.line((loc_list[i][0]-udl_margin, 0, loc_list[i][0]-udl_margin, img_height),fill = (255,0,0,0), width = udl_width)
-    
+            draw.line((loc_list[i][0]-udl_margin, tot_margin, loc_list[i][0]-udl_margin, img_height-tot_margin),fill = (255,0,0,0), width = udl_width)
+    add_border(draw)
     im.save('temp.pdf')
 
     merger = PdfFileMerger()
