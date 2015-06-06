dbbde20e1076fb9dc2c9bb42e1a36aea1c48c2c5
Liang Chen <chen348@indiana.edu>
2015-06-06 11:24:12 -0400

    pdf mode

--- anchipy/glob.py
+++ anchipy/glob.py
@@ -4,3 +4,10 @@ img_width = 0
 img_center = img_height/2
 udl_width = 1
 udl_margin = 1
+outter_border_margin = 2
+inner_border_margin = 1
+outter_border_width = 3
+inner_border_width = 1
+inner_margin = 2
+
+tot_margin = outter_border_margin + outter_border_width + inner_border_margin + inner_border_width + inner_margin
